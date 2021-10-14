c44b4bc3204a62e8e78c2ca01151102d736e58a4
FraMaffezzoli <francesco.maffezzoli@gmail.com>
2021-10-14 12:38:29 +0200

    Torch code
    Torch version on cnn, with updated enviroment and architecture files

--- src/architectures.py
+++ src/architectures.py
@@ -8,8 +8,11 @@ Image and Sound Processing Lab - Politecnico di Milano
 Paolo Bestagini
 """
 from tensorflow.keras import layers, models
+import torch.nn.functional as F
+from torch import nn
+import torch
 
-
+# Keras architecture
 def model_1(input_shape):
     """
     Simple CNN model
@@ -28,3 +31,27 @@ def model_1(input_shape):
     model.add(layers.Softmax())
 
     return model
+
+# Torch architecture
+class model_torch(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3)  # 30x30
+        self.pool1 = nn.MaxPool2d(2, 2)   # 15x15
+        self.conv2 = nn.Conv2d(32, 64, 3)  # 13x13
+        self.pool2 = nn.MaxPool2d(2, 2)    # 6x6, no padding
+        self.conv3 = nn.Conv2d(64, 64, 3)  # 4x4
+        self.dense1 = nn.Linear(4*4*64, 64)
+        self.dense2 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.dense1(x))
+        x = self.dense2(x)
+        return x
+
+
--- train_cnn_keras.py
+++ train_cnn_keras.py
@@ -14,16 +14,16 @@ from tensorflow.keras import datasets
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
 import src.architectures as arch
-from params import trained_models_root, model_name, input_shape, batch_size, epochs
+from params import trained_models_root, model_name_keras, input_shape, batch_size, epochs
 from src.utils import set_gpu
 
-set_gpu(id=-1)
+set_gpu(id=0)
 
 
 def main():
     # Define output paths
-    weights_path = os.path.join(trained_models_root, '{:s}'.format(model_name), 'model')
-    history_path = os.path.join(trained_models_root, '{:s}'.format(model_name), 'history.npy')
+    weights_path = os.path.join(trained_models_root, '{:s}'.format(model_name_keras), 'model')
+    history_path = os.path.join(trained_models_root, '{:s}'.format(model_name_keras), 'history_keras.npy')
 
     # Load dataset for training and validation
     (img_tr, y_tr), (img_val, y_val) = datasets.cifar10.load_data()
@@ -33,7 +33,7 @@ def main():
     img_val = img_val / 255.0
 
     # Initialize model
-    model = getattr(arch, model_name)(input_shape)
+    model = getattr(arch, model_name_keras)(input_shape)
     model.summary()
 
     # Callbacks
@@ -56,3 +56,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
